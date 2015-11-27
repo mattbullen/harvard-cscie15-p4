68173d8a89c82a84bda02c78312c7c94b0ca12b6
Matthew Bullen <burner0110@gmail.com>
2015-11-27 11:15:14 -0800

    .

---
+++ database/migrations/2015_11_27_191130_create_emails_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreateEmailsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->rememberToken();
+            $table->string('email')->unique();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('emails');
+    }
+
+}
