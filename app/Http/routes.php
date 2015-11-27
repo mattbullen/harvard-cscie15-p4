4890b002a8e094a1b7ba084cdc84cdabf4b83c95
Matthew Bullen <burner0110@gmail.com>
2015-11-27 11:05:04 -0800

    .

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,7 +30,7 @@ Route::post('/record/exercise/update', 'RecordController@updateExercise');
 Route::post('/record/exercise/delete', 'RecordController@deleteExercise');
 
 // For the table of users
-Route::post('/user', 'EmailController@handleUser');
+Route::post('/email', 'EmailController@handleUser');
 
 // Default route
 Route::get('/', function () {
