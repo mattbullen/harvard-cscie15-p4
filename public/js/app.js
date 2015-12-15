fed1a569037b43a3a177e33673cdd088d04b3d07
Matthew Bullen <burner0110@gmail.com>
2015-12-14 20:51:15 -0800

    .

--- public/js/app.js
+++ public/js/app.js
@@ -636,8 +636,8 @@ Polymer({
         // console.log("mappedData:", mappedData);
         
         // Define the min/max canvas ranges for x and y values
-        var x = d3.time.scale().range([0, paddedWidth]).domain(d3.extent(values, function (d) { return new Date(d.created_at); })); //.domain([model.getDate(minDate), model.getDate(maxDate)]);
-        var xBrush = d3.time.scale().range([0, paddedWidth]).domain(d3.extent(values, function (d) { return new Date(d.created_at); })); //.domain([model.getDate(minDate), model.getDate(maxDate)]);
+        var x = d3.time.scale().range([0, paddedWidth]).domain([model.getDate(minDate), model.getDate(maxDate)]);
+        var xBrush = d3.time.scale().range([0, paddedWidth]).domain([model.getDate(minDate), model.getDate(maxDate)]);
         var y = d3.scale.linear().range([height, 0]).domain([0, maxTotal]);
         
         // Define the x-axis dimensions, ticks, and orientation
